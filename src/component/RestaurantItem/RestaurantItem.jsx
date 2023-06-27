118510a69677cfea025a8a9607677071ea0ce6e8
Henish11 <henish09@gmail.com>
2023-06-27 20:04:39 +0530

    cart minor changes

--- src/component/RestaurantItem/RestaurantItem.jsx
+++ src/component/RestaurantItem/RestaurantItem.jsx
@@ -4,12 +4,11 @@ import {ReactComponent as NonvegIcon} from "../../assets/icons/nonveg.svg"
 import {ReactComponent as VegIcon} from "../../assets/icons/veg.svg"
 import { IoStarSharp } from "react-icons/io5"
 import { useDispatch } from "react-redux"
-import { addItem,increaseItem,decreaseItem } from "../../redux/cartSlice"
+import { addItem } from "../../redux/cartSlice"
 import { useState } from "react"
 
 
 const RestaurantItem =(props) =>{
-   console.log(props);
    const el = props.data
  
    const [quantity,setQuantity] = useState(0)
@@ -17,13 +16,6 @@ const RestaurantItem =(props) =>{
    const dispatch = useDispatch();
    const handleAddItem = (el)=>{
       dispatch(addItem(el))
-   }
-   const handleIncreaseItem = (el) =>{
-      dispatch(increaseItem(el));
-      setQuantity(el.card.info.inStock)
-   }  
-   const handleDecreaseItem = (el)=>{
-         dispatch(decreaseItem(el))
    }
 
     return(
@@ -43,12 +35,10 @@ const RestaurantItem =(props) =>{
                            {el?.card?.info?.imageId ? <img src={`${IMG_LINK}${el?.card?.info?.imageId}`} alt="item" /> : <div className="dummy-img">No Image</div> }
                         </div>
 
-                        { quantity < 1 ? (<button className="add-btn" onClick={()=>{ handleAddItem(el)}}> Add </button>) :
-                            (<div className="add-remove-btn">
-                                    <button onClick={()=>{handleDecreaseItem(el)}} className="remove">-</button>
-                                    <span>{el?.card?.info?.inStock}</span>
-                                    <button onClick={()=>{handleIncreaseItem(el)}}  className="add" >+</button>
-                            </div> )}
+                        { <button className= { quantity <= 0 ? "add-btn" : "add-btn added"} onClick={()=>{ 
+                           handleAddItem(el)
+                           setQuantity(1)
+                        }}> Add </button> }
                   </div>       
          </div>
         </>
