e1b7fb4efe6bbeb811a6ac9edda84cc4d41b40a6
Henish11 <henish09@gmail.com>
2023-06-27 11:41:44 +0530

    CORS Error Solved

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,15 +2,14 @@
 
 export const ALL_RESTAURANTS_API_LINK =
 // 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1417761&lng=72.77094149999999&page_type=DESKTOP_WEB_LISTING';
-'https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1417761&lng=72.77094149999999&page_type=DESKTOP_WEB_LISTING';
+'https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1417761&lng=72.77094149999999&page_type=DESKTOP_WEB_LISTING';
 
 export const ALL_LISTING_API_LINK =
-"https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1417761&lng=72.77094149999999&offset=0&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING"
+"https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1417761&lng=72.77094149999999&offset=0&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING"
 
 
 export const RESTAURANT_DETAILS_API =
-'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1417761&lng=72.77094149999999&restaurantId=';
+'https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1417761&lng=72.77094149999999&restaurantId=';
 
 
-export const IMG_LINK =
-	'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/';
+export const IMG_LINK = 'https://corsproxy.io/?https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/';
